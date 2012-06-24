5166227ba22494545e07853a292d0df788864ecd
George Ogata <george.ogata@gmail.com>
2012-06-24 12:41:24 -0400

    Enforce nullability, varchar_limits on #change_column.

--- lib/db_nazi/abstract_adapter.rb
+++ lib/db_nazi/abstract_adapter.rb
@@ -1,30 +1,54 @@
 module DBNazi
   module AbstractAdapter
-    def add_column(table_name, column_name, type, options = {})
-      if DBNazi.enabled?(:require_nullability)
-        options.key?(:null) or
-          raise NullabilityRequired, "[db_nazi] :null parameter required"
-      end
-      if DBNazi.enabled?(:require_varchar_limits)
-        # AR calls #to_sym on type, so do the same here.
-        type.to_sym == :string && !options.key?(:limit) and
-          raise VarcharLimitRequired, "[db_nazi] string column requires :limit parameter"
-      end
-      super
-    end
+    extend ActiveSupport::Concern
 
-    def add_index(table_name, column_name, options = {})
-      if DBNazi.enabled?(:require_index_uniqueness)
-        options.key?(:unique) or
-          raise IndexUniquenessRequired, "[db_nazi] :unique parameter required"
+    module ClassMethods
+      def new(*)
+        # We mix into singleton classes here, since some adapters define these
+        # methods directly in their classes (e.g. SQLite3Adapter#change_column),
+        # and we don't want to load these classes just to monkeypatch them.
+        super.tap do |adapter|
+          adapter.extend Adapter
+        end
       end
     end
 
-    def create_table(name, *)
-      if name.to_s == ActiveRecord::Migrator.schema_migrations_table_name.to_s
-        DBNazi.disable { super }
-      else
+    module Adapter
+      def add_column(table_name, column_name, type, options = {})
+        nazi_column_options(type, options)
         super
+      end
+
+      def add_index(table_name, column_name, options = {})
+        if DBNazi.enabled?(:require_index_uniqueness)
+          options.key?(:unique) or
+            raise IndexUniquenessRequired, "[db_nazi] :unique parameter required"
+        end
+      end
+
+      def create_table(name, *)
+        if name.to_s == ActiveRecord::Migrator.schema_migrations_table_name.to_s
+          DBNazi.disable { super }
+        else
+          super
+        end
+      end
+
+      def change_column(table_name, column_name, type, options = {})
+        nazi_column_options(type, options)
+        super
+      end
+
+      def nazi_column_options(type, options)
+        if DBNazi.enabled?(:require_nullability)
+          options.key?(:null) or
+            raise NullabilityRequired, "[db_nazi] :null parameter required"
+        end
+        if DBNazi.enabled?(:require_varchar_limits)
+          # AR calls #to_sym on type, so do the same here.
+          type.to_sym == :string && !options.key?(:limit) and
+            raise VarcharLimitRequired, "[db_nazi] string column requires :limit parameter"
+        end
       end
     end
   end
--- test/unit/db_nazi/test_abstract_adapter.rb
+++ test/unit/db_nazi/test_abstract_adapter.rb
@@ -8,36 +8,34 @@ describe DBNazi::AbstractAdapter do
     connection.create_table 'test_table'
   end
 
-  describe "nullability" do
-    describe "when it is required" do
+  describe "#add_column" do
+    describe "when nullability is required" do
       use_attribute_value DBNazi, :require_nullability, true
 
-      it "raises a DBNazi::NullabilityRequired if :null is not specified when adding a column" do
+      it "raises a DBNazi::NullabilityRequired if :null is not specified" do
         lambda do
           connection.add_column 'test_table', 'test_column', :boolean
         end.must_raise(DBNazi::NullabilityRequired)
       end
 
-      it "does not raise a DBNazi::NullabilityRequired if :null is true when adding a column" do
+      it "does not raise a DBNazi::NullabilityRequired if :null is true" do
         connection.add_column 'test_table', 'test_column', :boolean, null: true
       end
 
-      it "does not raise a DBNazi::NullabilityRequired if :null is false when adding a column" do
+      it "does not raise a DBNazi::NullabilityRequired if :null is false" do
         connection.add_column 'test_table', 'test_column', :boolean, null: false, default: false
       end
     end
 
-    describe "when it is not required" do
+    describe "when nullability is not required" do
       use_attribute_value DBNazi, :require_nullability, false
 
-      it "does not raise a DBNazi::NullabilityRequired if :null is not specified when adding a column" do
+      it "does not raise a DBNazi::NullabilityRequired if :null is not specified" do
         connection.add_column 'test_table', 'test_column', :boolean
       end
     end
-  end
 
-  describe "varchar limits" do
-    describe "when they are required" do
+    describe "when varchar limits are required" do
       use_attribute_value DBNazi, :require_varchar_limits, true
 
       it "raises a DBNazi::VarcharLimitRequired if :limit is not specified for a :string column" do
@@ -51,7 +49,7 @@ describe DBNazi::AbstractAdapter do
       end
     end
 
-    describe "when they are not required" do
+    describe "when varchar limits are not required" do
       use_attribute_value DBNazi, :require_varchar_limits, false
 
       it "does not raise a DBNazi::VarcharLimitRequired if :limit is not specified for a :string column" do
@@ -60,12 +58,66 @@ describe DBNazi::AbstractAdapter do
     end
   end
 
-  describe "index uniqueness" do
+  describe "#change_column" do
+    before do
+      connection.add_column 'test_table', 'test_column', :boolean, null: false, default: false
+    end
+
+    describe "when nullability is required" do
+      use_attribute_value DBNazi, :require_nullability, true
+
+      it "raises a DBNazi::NullabilityRequired if :null is not specified" do
+        lambda do
+          connection.change_column 'test_table', 'test_column', :boolean
+        end.must_raise(DBNazi::NullabilityRequired)
+      end
+
+      it "does not raise a DBNazi::NullabilityRequired if :null is true" do
+        connection.change_column 'test_table', 'test_column', :boolean, null: true
+      end
+
+      it "does not raise a DBNazi::NullabilityRequired if :null is false" do
+        connection.change_column 'test_table', 'test_column', :boolean, null: false, default: false
+      end
+    end
+
+    describe "when nullability is not required" do
+      use_attribute_value DBNazi, :require_nullability, false
+
+      it "does not raise a DBNazi::NullabilityRequired if :null is not specified" do
+        connection.change_column 'test_table', 'test_column', :boolean
+      end
+    end
+
+    describe "when varchar limits are required" do
+      use_attribute_value DBNazi, :require_varchar_limits, true
+
+      it "raises a DBNazi::VarcharLimitRequired if :limit is not specified for a :string column" do
+        lambda do
+          connection.change_column 'test_table', 'test_column', :string, null: true
+        end.must_raise(DBNazi::VarcharLimitRequired)
+      end
+
+      it "does not raise a DBNazi::VarcharLimitRequired if :limit is specified for a :string column" do
+        connection.change_column 'test_table', 'test_column', :string, limit: 255, null: true
+      end
+    end
+
+    describe "when varchar limits are not required" do
+      use_attribute_value DBNazi, :require_varchar_limits, false
+
+      it "does not raise a DBNazi::VarcharLimitRequired if :limit is not specified for a :string column" do
+        connection.change_column 'test_table', 'test_column', :string, null: true
+      end
+    end
+  end
+
+  describe "#add_index" do
     before do
       connection.add_column 'test_table', 'test_column', :boolean, null: true
     end
 
-    describe "when it is required" do
+    describe "when index uniqueness is required" do
       use_attribute_value DBNazi, :require_index_uniqueness, true
 
       it "raises a DBNazi::IndexUniquenessRequired if :unique is not specified for an index" do
@@ -83,7 +135,7 @@ describe DBNazi::AbstractAdapter do
       end
     end
 
-    describe "when it is not required" do
+    describe "when index uniqueness is not required" do
       use_attribute_value DBNazi, :require_index_uniqueness, false
 
       it "does not raise a DBNazi::IndexUniquenessRequired if :unique is not specified for an index" do
@@ -92,8 +144,10 @@ describe DBNazi::AbstractAdapter do
     end
   end
 
-  it "does not prevent construction of the schema migrations table" do
-    # AR doesn't specify a varchar limit for the version column. Lame.
-    connection.initialize_schema_migrations_table
+  describe "#initialize_schema_migrations_table" do
+    it "still works" do
+      # AR doesn't specify a varchar limit for the version column. Lame.
+      connection.initialize_schema_migrations_table
+    end
   end
 end
